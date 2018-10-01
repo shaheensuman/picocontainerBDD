189541b819c81acfd0cbc03dfa3b2e8897efb86d
shaheensuman <jshaheen03@gmail.com>
2018-09-30 22:56:00 -0400

    database

--- src/main/java/com/wholefoods/framework/database/DatabaseConnection.java
+++ src/main/java/com/wholefoods/framework/database/DatabaseConnection.java
@@ -1,19 +1,38 @@
 package com.wholefoods.framework.database;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DatabaseConnection  {
-    private DatabaseConnection instance;
+    private static DatabaseConnection instance;
     private Connection connection;
+    Properties prop=new Properties();
 
-    public DatabaseConnection() {
-        Properties properties;
+    private DatabaseConnection() throws SQLException, IOException {
+        prop.load(new FileInputStream("configs//databaseProperty.properties"));
         try {
-            Class.forName(" ");
-          //  this.connection= DriverManager.getConnection()
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection(prop.getProperty("databaseUrl"), prop.getProperty("databaseUserName"), prop.getProperty("databasePassword"));
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Database Connection Creation Failed : " + ex.getMessage());
         }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static DatabaseConnection getInstance() throws SQLException, IOException {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new DatabaseConnection();
+        }
+
+        return instance;
     }
 }
